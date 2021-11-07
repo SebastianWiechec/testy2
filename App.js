1863728d791a6305b77233afaf79f971e49101cb
Karol Rechman <39775135+KarolRechman@users.noreply.github.com>
2021-11-07 18:42:35 +0100

    Update App.js

--- App.js
+++ App.js
@@ -12,12 +12,12 @@ import { Navigation } from './src/infrastructure/navigation';
 import { AuthenticationContextProvider } from './src/services/authentication/AuthenticationContext';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAMOQP9M0--a-l6JDPF5wIXxSHtTd58BLw',
-  authDomain: 'yummeals-e8d3d.firebaseapp.com',
-  projectId: 'yummeals-e8d3d',
-  storageBucket: 'yummeals-e8d3d.appspot.com',
-  messagingSenderId: '572941448591',
-  appId: '1:572941448591:web:9993d5c64e60e32232c7fa',
+  apiKey: 'AIzaSyBZqBqmfgvSILa3OFHkvgwTI_3U2H4WtBk', //'AIzaSyAMOQP9M0--a-l6JDPF5wIXxSHtTd58BLw',
+  authDomain: 'icespotting.firebaseapp.com', //'yummeals-e8d3d.firebaseapp.com',
+  projectId: 'icespotting', //'yummeals-e8d3d',
+  storageBucket: 'icespotting.appspot.com', //'yummeals-e8d3d.appspot.com',
+  messagingSenderId: '729160709089', //'572941448591',
+  appId: '1:729160709089:android:7f55c031a5957c4cf258a0', //'1:572941448591:web:9993d5c64e60e32232c7fa',
 };
 
 if (!firebase.apps.length) {
