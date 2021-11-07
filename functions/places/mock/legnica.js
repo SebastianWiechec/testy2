ec62bf589c4332d166acc2b29232ae82353c472a
Sebastian W <61796880+SebastianWiechec@users.noreply.github.com>
2021-11-07 18:38:04 +0100

    Initial commit

---
+++ functions/places/mock/legnica.js
@@ -0,0 +1,51 @@
+module.exports = {
+  html_attributions: [],
+  next_page_token: 'some token',
+  results: [
+    {
+      business_status: 'OPERATIONAL',
+      geometry: {
+        location: {
+          lat: 51.2070067,
+          lng: 16.1553231,
+        },
+        viewport: {
+          northeast: {
+            lat: 51.24899153795081,
+            lng: 16.23309876357564,
+          },
+          southwest: {
+            lat: 51.16039123467405,
+            lng: 16.08756230747737,
+          },
+        },
+      },
+      icon: 'https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/restaurant-71.png',
+      name: 'Cos w Legnicy',
+      opening_hours: {
+        open_now: true,
+      },
+      photos: [
+        {
+          height: 478,
+          html_attributions: [''],
+          photo_reference: '',
+          width: 851,
+        },
+      ],
+      place_id: 'some place id 20',
+      plus_code: {
+        compound_code: '',
+        global_code: '',
+      },
+      price_level: 3,
+      rating: 4.6,
+      reference: '',
+      scope: '',
+      types: ['restaurant', 'food', 'icecream', 'establishment'],
+      user_ratings_total: 151,
+      vicinity: 'Legnica, centrum',
+    },
+  ],
+  status: 'OK',
+};
